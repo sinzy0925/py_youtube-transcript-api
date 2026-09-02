a6bf86ea90fdd243884d60d087f3d609f0d8970e
sinzy0925 <sinzy0925@gmail.com>
2026-09-02 20:50:49 +0900

    git pull add commit push

---
+++ run_channels.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+./run_channel.sh 'https://www.youtube.com/@invest_study/videos' --fromto 0:2 --foreground
+./run_channel.sh 'https://www.youtube.com/@ga-ko/videos' --fromto 0:2 --foreground
+./run_channel.sh 'https://www.youtube.com/@you___toushi/videos' --fromto 0:2 --foreground
+./run_git_pull_add_commit_push.ps1
